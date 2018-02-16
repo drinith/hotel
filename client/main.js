3300c909b5b2189f558cdc0d113ae455f783b7e7
Instrutor <felpmel@gmail.com>
2018-02-16 11:47:20 -0200

    COmitando arquivos

--- client/main.js
+++ client/main.js
@@ -3,20 +3,20 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
+Template.cadastrar.onCreated(function cadastrarOnCreated() {
   // counter starts at 0
   this.counter = new ReactiveVar(0);
 });
 
-Template.hello.helpers({
+Template.cadastrar.helpers({
   counter() {
     return Template.instance().counter.get();
   },
 });
 
-Template.hello.events({
+Template.cadastrar.events({
   'click button'(event, instance) {
     // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
+    alert("teste");
   },
 });
